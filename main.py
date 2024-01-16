cd89afb459cf72f701d2201e7417b0b6f2f1e254
NorthernChicken <144752748+NorthernChicken@users.noreply.github.com>
2024-01-15 20:54:22 -0800

    bugfixes

--- main.py
+++ main.py
@@ -21,7 +21,9 @@ api_link = api_base + ip
 while True:
     try:
         response = requests.get(api_link)
-
+        
+        current_datetime = datetime.now()
+        formatted_datetime = current_datetime.strftime("%Y-%m-%d %H:%M:%S")
         if response.status_code == 200:
             json_data = response.json()
             players_online = json_data.get("players", {}).get("online", 0)
@@ -32,8 +34,6 @@ while True:
                 formatted_status = "Offline"
             else:
                 formatted_status = "Unknown"
-            current_datetime = datetime.now()
-            formatted_datetime = current_datetime.strftime("%Y-%m-%d %H:%M:%S")
             print("*---------------------------------")
             print(f"{bold_start}{formatted_datetime}{bold_end}")
             print("Server Status:", formatted_status)
