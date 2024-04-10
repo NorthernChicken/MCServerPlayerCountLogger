8160a4cfcbc055aec659b9f3b8152000c0263151
skonk <anntvfamily@gmail.com>
2024-04-10 14:22:30 -0700

    update counter
    adds a countdown to the update, removes long list to save space in the console (just look at the log lmao)

--- playercount.py
+++ playercount.py
@@ -2,11 +2,14 @@ import requests
 from datetime import datetime
 import time
 import ctypes
+import os
 
 '''
 * Fork of NorthernChicken's player count logger by skokcoder
 * Desc: Continutally checks the amount of players on a Minecraft server using mcstatus.io's API, displays a message when a specified amount of people are online, then stops
 '''
+
+countdown = 0
 
 with open('ip.txt', 'r') as file:
     ip = file.read()
@@ -22,19 +25,22 @@ bold_end = "\033[0m"
 
 while True:
     response = requests.get(api_link)
+    os.system("cls")
     if response.status_code == 200:
-        json_data = response.json()
-        players_online = json_data.get("players", {}).get("online", 0)
-        player_list = json_data.get("players", {}).get("list", 0)
-        online_status = json_data['online']
-        if online_status:
-            formatted_status = "Online"
-        elif not online_status:
-            formatted_status = "Offline"
-        else:
-            formatted_status = "Unknown"
-        current_datetime = datetime.now()
-        formatted_datetime = current_datetime.strftime("%Y-%m-%d %H:%M:%S")
+        if countdown == 0:
+            json_data = response.json()
+            players_online = json_data.get("players", {}).get("online", 0)
+            player_list = json_data.get("players", {}).get("list", 0)
+            online_status = json_data['online']
+            if online_status:
+                formatted_status = "Online"
+            elif not online_status:
+                formatted_status = "Offline"
+            else:
+                formatted_status = "Unknown"
+            current_datetime = datetime.now()
+            formatted_datetime = current_datetime.strftime("%Y-%m-%d %H:%M:%S")
+            countdown = 5
         print("*---------------------------------")
         print(f"{bold_start}{formatted_datetime}{bold_end}")
         print("Server Status:", formatted_status)
@@ -43,13 +49,15 @@ while True:
             print(f"Server Recon is not turned on.")
         else:
             print(f"Player List: {player_list}")
+        print(f"Updating in {countdown} seconds.")
         print("*---------------------------------")
         with open("log.txt", 'a') as file:
             file.write(formatted_datetime + " Server is " + formatted_status + ". " + str(players_online) + f" players, player list: {player_list}\n")
         if players_online >= targetamt:
             ctypes.windll.user32.MessageBoxW(0, f"Your specified player amount ({targetamt}) has been reached.", "!! TARGET AMOUNT REACHED !!", 0)
             break
-        time.sleep(3)
+        time.sleep(1)
+        countdown -= 1
     else:
         print("Error:", response.status_code)
         time.sleep(5)
